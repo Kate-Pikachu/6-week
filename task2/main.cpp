2d36050b802791f60b8692801a2ca70849f16fca
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-03-19 20:30:17 +0300

    2 задача

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <string>
 
 
 int main() {
 	
 	std::ofstream file("set.txt");
 
+	const int size = 10;
+
 	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			file << "line number: " << j << i << std::endl;;
+		for (int j = 0; j < size; j++) {
+			file << "line number " << j << ": " << i << std::endl;;
 		}
 		file << '\n';
 	}
@@ -20,8 +23,14 @@ int main() {
 
 	std::ifstream ffile("set.txt");
 
+	if (n > 1)
+		ffile.seekg((n - 1) * (size + 2), std::ios::beg);
+	else
+		ffile.seekg(0); // at the begining of the file
 
-
+	std::string str;
+	getline(ffile, str);
+	std::cout << "line: " << str << std::endl;
 
 	system("pause");
 	return EXIT_SUCCESS;
