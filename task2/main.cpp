06e7e0e35cb358659c59451998572e4fdabfeb9b
Kate-Pikachu <pikulina.ev@phystech.edu>
2021-03-19 19:23:11 +0300

    not full yet

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
 
 int main() {
+	
+	std::ofstream file("set.txt");
 
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			file << "line number: " << j << i << std::endl;;
+		}
+		file << '\n';
+	}
+
+	int n;
+	std::cout << "Enter number of line: ";
+	std::cin >> n;
+
+	std::ifstream ffile("set.txt");
+
+
+
+
+	system("pause");
+	return EXIT_SUCCESS;
 }
